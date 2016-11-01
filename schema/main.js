e668eb08f4158a3400dac0eb5116559708bc04cc
masterfung <thung@me.com>
2016-11-01 00:10:50 -0400

    user count from db

--- schema/main.js
+++ schema/main.js
@@ -1,41 +1,47 @@
 const {
-     GraphQLSchema,
-     GraphQLObjectType,
-     GraphQLString,
-     GraphQLInt,
-     GraphQLList
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList
 } = require('graphql');
 
 const roll = () => Math.floor(6 * Math.random()) + 1;
-   
+
 const queryType = new GraphQLObjectType({
-     name: 'RootQuery',
-     fields: {
-       hello: {
-         type: GraphQLString,
-         resolve: () => 'world'
-		},
+    name: 'RootQuery',
+    fields: {
+        hello: {
+            type: GraphQLString,
+            resolve: () => 'world'
+        },
         diceRoll: {
-         type: new GraphQLList(GraphQLInt), // defining the type
-         args: {
-            count: { 
-                type: GraphQLInt,
-                defaultValue: 3
-             }
-         },
-         resolve: (_, args) => {
-            let rolls = [];
-            for (let i = 0; i < args.count; i++) {
-                rolls.push(roll());
+            type: new GraphQLList(GraphQLInt), // defining the type
+            args: {
+                count: {
+                    type: GraphQLInt,
+                    defaultValue: 3
+                }
+            },
+            resolve: (_, args) => {
+                    let rolls = [];
+                    for (let i = 0; i < args.count; i++) {
+                        rolls.push(roll());
+                    }
+                    return rolls;
+                } // two random numbers
+        },
+        usersCount: {
+            type: GraphQLInt,
+            resolve: (_, args, { db }) => {
+                db.collection('users').count()
             }
-            return rolls;
-         } // two random numbers
-        } 
-	}
+        }
+    }
 });
 
 const mySchema = new GraphQLSchema({
-     query: queryType
+    query: queryType
 });
 
 module.exports = mySchema;
