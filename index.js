1458eb8c096a18f9a8c2b7bfb5d9598c38adee3a
masterfung <thung@me.com>
2016-11-01 00:11:45 -0400

    user counts

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const { graphql } = require('graphql');
 const readline = require('readline');
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
+const MONGO_URL = process.env.MONGO_URL;
 const mySchema = require('./schema/main');
 
-const rli = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+MongoClient.connect(MONGO_URL, (err, db) => {
+	assert.equal(null, err);
+	console.log('You are connected to MongoDB server');
 
-rli.question('Client Request: ', inputQuery => {
-	graphql(mySchema, inputQuery).then(res => {
-
-		console.log('Server Answer: ', res.data);
+	const rli = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
 	});
 
-	rli.close();
-})
+	rli.question('Client Request: ', inputQuery => {
+		graphql(mySchema, inputQuery, {}, { db }).then(res => {
+
+			console.log('Server Answer: ', res.data);
+			db.collection('users').count()
+     		  .then(usersCount => console.log(usersCount));
+
+			db.close(() => rli.close());
+		});
+
+	});
+});
+
