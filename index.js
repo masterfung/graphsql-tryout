b412ea9c5ead6de294343fc0389bd716ed3273f5
masterfung <thung@me.com>
2016-11-01 10:05:06 -0400

    brought in express

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { graphql } = require('graphql');
 const readline = require('readline');
 const { MongoClient } = require('mongodb');
 const assert = require('assert');
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
+
+const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
 const mySchema = require('./schema/main');
@@ -9,6 +13,15 @@ const mySchema = require('./schema/main');
 MongoClient.connect(MONGO_URL, (err, db) => {
 	assert.equal(null, err);
 	console.log('You are connected to MongoDB server');
+
+	app.use('graphql', graphqlHTTP({
+		schema: mySchema,
+		context: { db }
+	}));
+
+	app.listen(3000, () =>
+		console.log('Express is running on port 3000')
+	);
 
 	const rli = readline.createInterface({
 		input: process.stdin,
@@ -25,4 +38,6 @@ MongoClient.connect(MONGO_URL, (err, db) => {
 
 	});
 });
+
+
 
