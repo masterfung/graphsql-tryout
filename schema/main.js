0d5c308480d5b202ae16465f8a1cf906f5526d11
masterfung <thung@me.com>
2016-11-02 01:00:06 -0400

    employee type and some small updates

--- schema/main.js
+++ schema/main.js
@@ -3,40 +3,66 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
+    GraphQLBoolean,
     GraphQLList
 } = require('graphql');
 
 const roll = () => Math.floor(6 * Math.random()) + 1;
 
-const queryType = new GraphQLObjectType({
-    name: 'RootQuery',
-    fields: {
-        hello: {
+const exEmployee = {
+    firstName: 'joseph',
+    lastName: 'mitch'
+};
+
+const EmployeeType = new GraphQLObjectType({
+    name: 'Employee',
+    fields: () => ({
+        name: {
             type: GraphQLString,
-            resolve: () => 'world'
-        },
-        diceRoll: {
-            type: new GraphQLList(GraphQLInt),
             args: {
-                count: {
-                    type: GraphQLInt,
-                    defaultValue: 3
+                upperCase: { type: GraphQLBoolean },
+                resolve: (obj, args) => {
+                    let fullName = `${obj.firstName} ${obj.lastName}`;
+                    return args.upperCase ? fullName.toUpperCase() : fullName;
                 }
             },
-            resolve: (_, args) => {
-                let rolls = [];
-                for (let i = 0; i < args.count; i++) {
-                    rolls.push(roll());
-                }
-                return rolls;
-            }
-        },
-        usersCount: {
-            type: GraphQLInt,
-            resolve: (_, args, { db }) =>
-                db.collection('users').count()
+            boss: { type: EmployeeType }
         }
-    }
+    })
+});
+
+const queryType = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: () => ({
+            hello: {
+                description: 'Resolves the str world',
+                type: GraphQLString,
+                resolve: () => 'world'
+            },
+            diceRoll: {
+                description: '**Simulate** a dice roll determined by count input',
+                type: new GraphQLList(GraphQLInt),
+                args: {
+                    count: {
+                        type: GraphQLInt,
+                        defaultValue: 3
+                    }
+                },
+                resolve: (_, args) => {
+                    let rolls = [];
+                    for (let i = 0; i < args.count; i++) {
+                        rolls.push(roll());
+                    }
+                    return rolls;
+                }
+            },
+            usersCount: {
+                description: 'Shows the total users in the MongoDB',
+                type: GraphQLInt,
+                resolve: (_, args, { db }) =>
+                    db.collection('users').count()
+            }
+        })
 });
 
 const mySchema = new GraphQLSchema({
